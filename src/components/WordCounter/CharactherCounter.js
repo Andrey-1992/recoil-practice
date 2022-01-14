4ce13382c2c00fd1dc092041c58d2beaf8470929
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-13 18:03:31 -0600

    Incorporate counter state for first exercise

--- src/components/WordCounter/CharactherCounter.js
+++ src/components/WordCounter/CharactherCounter.js
@@ -1,4 +1,29 @@
+import { useRecoilState } from "recoil"
+
 const textState = atom({
   key: 'textState',
   default: '',
 })
+
+export const characterCounter = () => {
+  return (
+    <TextInput /> 
+    <characterCounter />
+  )
+}
+
+export const textInput = () => {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type='text' value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  )
+}
