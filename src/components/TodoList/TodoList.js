d14795c832d61d81aeb7674e45783891a759ce97
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-13 22:04:32 -0600

    Add export default

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,18 @@
+import { useRecoilValue } from "recoil"
+import { todoListState} from './Atom'
+
+export const TodoList = () => {
+  const todolist = useRecoilValue(todoListState);
+
+  return (
+    <>
+      {/* <TodoListStats /> */}
+      {/* <TodoListFilters /> */}
+      <TodoItemCreator />
+
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
+  );
+}
