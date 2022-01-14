49a065d8c6c20d1d703b531d3ed0ee0db6378683
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-13 22:12:00 -0600

    Set up TodoitemCreator file prescription for the file

---
+++ src/components/TodoList/TodoItemCreator.js
@@ -0,0 +1,37 @@
+import { useSetRecoilState } from "recoil";
+import React, { useState } from 'react';
+import { todoListState} from './Atom';
+
+// utility for creating unique Id
+let id = 0;
+function getId() {
+  return id++;
+}
+
+export const TodoItemCreator = () => {
+  const [inputValue, setInputValue] = useState('');
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue('');
+  };
+
+  const onChange = ({target: {value}}) => {
+    setInputValue(value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+}
